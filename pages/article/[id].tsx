33a07301520ef60d643ac7d65034cac39d243716
Keith Yao <i@yaofur.com>
2022-06-01 02:21:44 +0200

    yo, cool

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { NextPageContext } from 'next'
 import debug from 'debug'
 import { articleCollection } from 'models/client'
@@ -6,7 +7,7 @@ import { htmlToPortableText } from 'models/html2portabletext'
 import type { PortableTextBlock } from '@portabletext/types'
 import { PortableText } from '@portabletext/react'
 
-const log = debug('article.ssr')
+const log = debug('ArticleView')
 
 const ArticleView = ({
   id,
@@ -18,6 +19,50 @@ const ArticleView = ({
   portableText: PortableTextBlock[]
 }) => {
   const usePortable = false
+  const PAGE_GAP = 32
+
+  const frameRef = useRef<HTMLDivElement>(null)
+
+  const [[currentPage, totalPage], setPage] = useState<[number, number]>([0, 1])
+
+  useEffect(() => {
+    if (!frameRef.current) {
+      return
+    }
+    const node = frameRef.current
+
+    const totalPage = Math.round(
+      (node.scrollWidth - node.offsetWidth) / node.offsetWidth + 1
+    )
+
+    setPage([0, totalPage])
+  }, [frameRef, setPage])
+
+  const nextPage = useCallback(() => {
+    if (currentPage >= totalPage - 1) {
+      setPage([totalPage - 1, totalPage])
+    } else {
+      setPage([currentPage + 1, totalPage])
+    }
+  }, [currentPage, totalPage])
+
+  const prevPage = useCallback(() => {
+    if (currentPage <= 0) {
+      setPage([0, totalPage])
+    } else {
+      setPage([currentPage - 1, totalPage])
+    }
+  }, [currentPage, totalPage])
+
+  useEffect(() => {
+    if (!frameRef.current) {
+      return
+    }
+
+    debug('goingTo')(currentPage)
+
+    frameRef.current.scrollTo(frameRef.current.offsetWidth * currentPage, 0)
+  }, [currentPage])
 
   if (!doc) {
     return <div>no article for {id}</div>
@@ -25,14 +70,26 @@ const ArticleView = ({
 
   return (
     <div
-      className='container p-4 mx-auto font-serif prose h-screen overflow-scroll'
+      className='container p-4 mx-auto font-serif prose h-screen overflow-hidden'
       style={{
-        columns: '393px 1',
-        columnGap: 32
+        columns: `${frameRef?.current?.offsetWidth || 100}px 1`,
+        columnGap: PAGE_GAP,
       }}
+      ref={frameRef}
+      onPointerUp={(e) => {
+        if (!frameRef.current) return
+        const frameWidth = frameRef.current.offsetWidth
 
-      onClick
-
+        if (e.clientX < frameWidth / 3) {
+          console.log('pageLeft', e.clientX, frameWidth)
+          prevPage()
+        } else if (e.clientX > (frameWidth / 3) * 2) {
+          console.log('pageRight', e.clientX, frameWidth)
+          nextPage()
+        } else {
+          console.log('just a click')
+        }
+      }}
     >
       <h2>{doc.title}</h2>
 
@@ -51,12 +108,16 @@ const ArticleView = ({
           </div>
         </div>
       )}
+
+      <div className='fixed bottom-0 right-0'>
+        {currentPage + 1} / {totalPage}
+      </div>
     </div>
   )
 }
 
 export async function getServerSideProps(ctx: NextPageContext) {
-  // debug('article.ssr')('id', ctx.query.id)
+  const log = debug('article.ssr')
 
   try {
     log('getDoc', ctx.query.id)
