9407d91e7071a18413338b3cdc8c84a17d394617
Keith Yao <i@yaofur.com>
2022-06-02 22:16:15 +0200

    add tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,9 +14,10 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = '/api/trpc'
+    // const url = process.env.VERCEL_URL
+    //   ? `https://${process.env.VERCEL_URL}/api/trpc`
+    //   : 'http://localhost:3000/api/trpc';
 
     return {
       url,
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,24 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          id='matomo-script'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//mavfew.fra1.a.restack.io/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();`,
+          }}
+        />
       </body>
     </Html>
   )
