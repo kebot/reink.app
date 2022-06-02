fd32061224b467ef6ff0cbbe51bf2bfa99e322fb
Keith Yao <i@yaofur.com>
2022-06-02 23:11:13 +0200

    init ory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
-import { withTRPC } from '@trpc/next';
-import { AppRouter } from './api/trpc/[trpc]';
+import { withTRPC } from '@trpc/next'
+import { AppRouter } from './api/trpc/[trpc]'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id='matomo-script'>
+        {`
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//mavfew.fra1.a.restack.io/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default withTRPC<AppRouter>({
@@ -25,7 +45,7 @@ export default withTRPC<AppRouter>({
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
-    };
+    }
   },
   /**
    * @link https://trpc.io/docs/ssr
