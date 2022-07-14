5d5e36f06b7bd5f9b6031861617bc6be39b616fa
Keith Yao <i@yaofur.com>
2022-07-14 17:08:00 +0200

    set the right url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,8 +20,8 @@ export default withTRPC<AppRouter>({
      * @link https://trpc.io/docs/ssr
      */
     // const url = '/api/trpc'
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : 'http://localhost:3000/api/trpc';
 
     return {
