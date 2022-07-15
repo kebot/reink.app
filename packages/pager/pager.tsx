87c2f81b2174aca86929a62d0782f2cc2ce2cd62
Keith Yao <i@yaofur.com>
2022-07-15 20:39:43 +0200

    set correct height

--- packages/pager/pager.tsx
+++ packages/pager/pager.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect, useCallback, PointerEventHandler } from 'react'
 import debug from 'debug'
-import { useKey } from 'react-use'
+import { useKey, useWindowSize } from 'react-use'
 import { useSwipeable } from 'react-swipeable'
 
 const log = debug('Pager')
@@ -13,12 +13,15 @@ function getFrameWidth(el: HTMLElement) {
   return el.getBoundingClientRect().width - PAGE_PADDING * 2 + PAGE_GAP
 }
 
+// fact: window.innerHeight will be the actually 100vh for mobile devices
+
 export const Pager = ({ children, menu }: { children: React.ReactNode; menu: React.ReactNode }) => {
   log('start-render')
 
   const frameRef = useRef<HTMLDivElement>(null)
   const [[currentPage, totalPage], setPage] = useState<[number, number]>([0, 1])
   const [menuVisible, setMenuVisible] = useState(false)
+  const {width, height} = useWindowSize();
 
   useEffect(() => {
     if (!frameRef.current) {
@@ -91,13 +94,15 @@ export const Pager = ({ children, menu }: { children: React.ReactNode; menu: Rea
     }
   }
 
+
   return (
     <div {...handlers}>
       <div
-        className='container p-4 mx-auto h-screen overflow-hidden'
+        className='container p-4 mx-auto overflow-hidden'
         style={{
           columns: `${frameRef?.current?.offsetWidth || 100}px 1`,
           columnGap: PAGE_GAP,
+          height: height
         }}
         ref={frameRef}
         onPointerUp={handleTap}
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,5 @@ module.exports = {
   },
 
   // tweak some browsers
-  important: true,
+  // important: true,
 }
