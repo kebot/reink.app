ef3b1d509197970bf818a3e18905c978fa67761e
Keith Yao <i@yaofur.com>
2023-12-03 03:23:48 +0100

    cleanup

--- src/packages/html2portabletext/html2portabletext.test.ts
+++
@@ -1,9 +0,0 @@
-import { htmlToPortableText } from './html2portabletext';
-
-describe('portabletext', () => {
-  it('should be fine', () => {
-    expect(
-      htmlToPortableText('<p>Hello</p>')
-    ).toMatchSnapshot()
-  })
-})
